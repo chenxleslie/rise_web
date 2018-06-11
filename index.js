16b23d3498d1382acc1ec70c3afb3419802cd324
Leslie Chen <lchen@squareup.com>
2018-06-11 00:05:14 -0700

    transit API

--- index.js
+++ index.js
@@ -26,4 +26,31 @@ function leslieWeatherLoad() {
     }
 }
 
+// 511 Transit API
+
+function tommyTransitLoad() {
+    var requestURL = 'http://api.511.org/transit/StopMonitoring?api_key=bd4b1c1e-7e9e-4a4f-a3b5-80f7c8ad4aea&agency=CT&format=json';
+    var request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function() {
+        var transit = request.response;
+        var transitJSON = JSON.stringify(transit);
+        document.getElementById("tommy-transit").innerHTML = transitJSON;
+    }
+}
+
+function leslieTransitLoad() {
+    var requestURL = 'http://api.511.org/transit/StopMonitoring?api_key=bd4b1c1e-7e9e-4a4f-a3b5-80f7c8ad4aea&agency=AC&format=json';
+    var request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function() {
+        var transit = request.response;
+        var transitJSON = JSON.stringify(transit);
+        document.getElementById("leslie-transit").innerHTML = transitJSON;
+    }
+}
 
